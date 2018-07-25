7fd48233fb3137f23d80add8e466c6c9c055bfe9
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2018-07-25 16:46:48 +0600

    Update check google play store app version and go to play store for update if update are avalable

--- app/src/main/java/madbarsoft/com/testapp/MainActivity.java
+++ app/src/main/java/madbarsoft/com/testapp/MainActivity.java
@@ -1,22 +1,66 @@
 package madbarsoft.com.testapp;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
+    String currentVersion, latestVersion;
+    Dialog dialog;
     Button btnSecondPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getCurrentVersion();
+        if(isOnline()) {
+            try {
+                latestVersion = new GetLatestVersion().execute().get();
+                if(!currentVersion.equals(latestVersion)){
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("More content has added !\nPlease update...");
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Click button action
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=madbarsoft.com.testapp")));
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.setCancelable(false);
+                    builder.show();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Toast.makeText(this, "Cur: "+currentVersion+" Latest: "+latestVersion, Toast.LENGTH_SHORT).show();
+
 
         btnSecondPage = findViewById(R.id.btnSecondPage);
-
         btnSecondPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,4 +71,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void getCurrentVersion(){
+        PackageManager pm = this.getPackageManager();
+        PackageInfo pInfo = null;
+
+        try {
+            pInfo =  pm.getPackageInfo(this.getPackageName(),0);
+
+        } catch (PackageManager.NameNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        currentVersion = pInfo.versionName;
+
+    }
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private class GetLatestVersion extends AsyncTask<String, String, String> {
+        String latestVersion;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Document doc = Jsoup.connect("https://play.google.com/store/apps/details?id=madbarsoft.com.testapp").get();
+                Element element = doc.getElementsByClass("BgcNfc").get(3);
+                latestVersion = element.parent().children().get(1).children().text();
+            } catch (Exception e) {
+                latestVersion = currentVersion;
+            }
+            return latestVersion;
+        }
+    }
+
 }
+
+
+
+
---
+++ app/src/main/java/madbarsoft/com/testapp/TestAppService.java
@@ -0,0 +1,6 @@
+package madbarsoft.com.testapp;
+
+public class TestAppService {
+
+
+}
